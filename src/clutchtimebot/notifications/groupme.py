7ebf2e853d7b489ceaec056eb50fd80ef4af575b
Brian Walheim <bwalheim1205@vt.edu>
2024-11-04 17:47:30 -0500

    Adding GroupMe notificationn type

---
+++ src/clutchtimebot/notifications/groupme.py
@@ -0,0 +1,16 @@
+from clutchtimebot.notifications.base import Notification
+import requests
+import os
+
+
+class GroupMeNotification(Notification):
+    BASE_URL = "https://api.groupme.com/v3/bots/post"
+
+    def __init__(self, bot_id: str = None):
+        if bot_id is None:
+            bot_id = os.getenv("GROUPME_BOT_ID")
+        self.bot_id = bot_id
+
+    def send(self, message):
+        data = {"bot_id": self.bot_id, "text": message}
+        requests.post(url=self.BASE_URL, json=data)
