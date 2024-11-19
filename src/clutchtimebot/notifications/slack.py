9901611012fd7f683938c44c22edd6ff48fed491
Brian Walheim <bwalheim1205@vt.edu>
2024-11-18 21:47:32 -0500

    Adding SlackBot implementation

---
+++ src/clutchtimebot/notifications/slack.py
@@ -0,0 +1,24 @@
+from clutchtimebot.notifications.base import Notification
+from slack_sdk import WebClient
+import os
+
+
+class SlackNotification(Notification):
+    def __init__(self, token: str = None, channel: str = None):
+        if token is None:
+            token = os.getenv("SLACK_TOKEN")
+        self.token = token
+
+        if channel is None:
+            channel = os.getenv("SLACK_CHANNEL")
+        self.channel = channel
+
+        self.client = WebClient(token=self.token)
+
+    def send(self, message):
+        self.client.chat_postMessage(channel="#general", text=message)
+
+
+if __name__ == "__main__":
+    notification = SlackNotification()
+    notification.send("test")
