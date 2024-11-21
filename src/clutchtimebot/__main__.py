6180cd795fde66862665d482bd639715a5336587
Brian Walheim <bwalheim1205@vt.edu>
2024-11-20 19:15:02 -0500

    Initial config parser implementation

--- src/clutchtimebot/__main__.py
+++ src/clutchtimebot/__main__.py
@@ -1,23 +1,37 @@
 from clutchtimebot.clutch_alerts import ClutchAlertsService
 from clutchtimebot.notifications.groupme import GroupMeNotification
+from clutchtimebot.config_parser import ConfigParser
+from clutchtimebot.notification_collector import NotificationCollector
 
 import argparse
+import os
 
 if __name__ == "__main__":
+    # Parse Arguments
     parser = argparse.ArgumentParser(description="Choose a notification type.")
     parser.add_argument(
-        "--type",
+        "-f" "--file",
+        dest="file",
         type=str,
-        required=False,
-        default="groupme",
-        choices=["groupme"],
-        help="The type of notifcations to use (Default: groupme).",
+        required=True,
+        help="Path to the YAML config file",
     )
-
     args = parser.parse_args()
 
-    if args.type == "groupme":
-        notification = GroupMeNotification()
+    # Collect Notification Classes
+    collector = NotificationCollector()
+    notifcation_dir = os.path.dirname(__file__) + "/notifications"
+    collector.collect_notifications(notifcation_dir)
 
-    alert_service = ClutchAlertsService(notification=notification)
+    # Parse Config
+    parser = ConfigParser(
+        args.file, collector.classname_dict, collector.common_name_dict
+    )
+    parser.parse_config()
+
+    alert_service = ClutchAlertsService(
+        notifications=parser.notifications,
+        db_path=parser.db_file_path,
+        db_table_name=parser.db_table_name,
+    )
     alert_service.run()
