5dfe40b980a7f24ed03585e0ddd3699c2f19a377
Brian Walheim <bwalheim1205@vt.edu>
2025-11-10 22:05:46 -0500

    Hot fix: cache issue with cdn.nba.com causing delays in score updates

--- src/clutchtimealerts/scraper/live_scores.py
+++ src/clutchtimealerts/scraper/live_scores.py
@@ -5,6 +5,17 @@ class NBAScoreScraper:
     BASE_URL = "https://cdn.nba.com/static/json/liveData/"
     ENDPOINT = "scoreboard/todaysScoreboard_00.json"
 
+    # Mimic web browser headers
+    HEADERS = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Cache-Control": "max-age=0",
+        "Connection": "keep-alive",
+        "Host": "cdn.nba.com",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+    }
+
     def __init__(self):
         """
         Initialize the NBAScoreScraper. This sets the URL that will be used to
